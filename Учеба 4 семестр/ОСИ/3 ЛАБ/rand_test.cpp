ef4a12c5d9ba05ae7ec7d17fb57b9b303e0d9b02
Ilya <ekov4work@outlook.com>
2026-04-24 17:09:06 +0300

    vault backup: 2026-04-24 17:09:06

---
+++ Учеба 4 семестр/ОСИ/3 ЛАБ/rand_test.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int threads = 1;
+    int n = 25; // 1 миллион элементов
+
+    FILE* f = fopen("input.txt", "w");
+    fprintf(f, "%d\n", threads);
+    fprintf(f, "%d\n", n);
+
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(f, "%d ", rand());
+    }
+    fclose(f);
+    return 0;
+}
